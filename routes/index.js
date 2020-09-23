45302dca55d3c23403af9dcd3cf0af069f6534d8
gagi <gagi.shmagi@gmail.com>
2020-09-23 11:56:39 +0300

    project 23.09 state

--- routes/index.js
+++ routes/index.js
@@ -44,6 +44,69 @@ router.post('/books/add', function (req, res, next) {
 });
 
 
+router.get('/book/edit/:id', function (req, res, next) {
+
+  let id = req.params.id;
+
+
+  var sql_string = 'SELECT * FROM Books WHERE Book_ID=?';
+
+  db.get(sql_string, id , (err, row) => {
+    if (err)
+      console.log(err.message);
+    else{
+      res.render('books/edit-book', {
+        title: 'Edit the book',
+        book: row
+      });
+    }
+  });
+
+});
+
+router.post('/books/edit/:id', function (req, res, next) {
+
+  let id = req.params.id;
+
+  console.log(req.body);
+  console.log(`Edit book with id ${id} request`);
+
+  const sql = "UPDATE Books SET Title=? , Author = ? , Comments = ? WHERE Book_ID = ? ";
+  const book = [req.body.Title, req.body.Author, req.body.Comments, id];
+
+  db.run(sql, book, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect(`/books`);
+  });
+
+});
+
+router.get('/book/delete/:id', function (req, res, next) {
+
+  let id = req.params.id;
+
+  let compfirmed = confirm("Are you sure?");
+
+  const sql = "DELETE FROM Books WHERE Book_ID=?";
+
+  if(compfirmed === true){
+    db.run(sql, id, err => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.redirect(`/books`);
+    });
+
+  }else
+    return;
+
+});
+
+
+
+
 router.get('/contact-us', function (req, res, next) {
   res.render('contact-us', {
     title: 'Contact Us'
