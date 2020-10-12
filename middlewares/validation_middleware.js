6c4a416b70e89448a85d25849af9dbdf06f3492d
gagi <gagi.shmagi@gmail.com>
2020-10-12 17:38:06 +0300

    Added fix for multiple errors instead of one

--- middlewares/validation_middleware.js
+++ middlewares/validation_middleware.js
@@ -1,16 +1,16 @@
 const Joi = require('joi');
 const middleware = (schema) => {
     return (req, res, next) => {
-        const { error } = schema.validate(req.body)
+        const { error } = schema.validate(req.body,{ abortEarly: false })
         const valid = error == null;
 
         if(valid){
             next();
         }else{
             const { details } = error
-            const message = details.map( item => item.message ).join(',')
+            const message = (details || []).map( item => item.message ).join(',')
 
-            console.log("error", message);
+            console.log("error", error);
             // res.status(422).json({ error: message})
             res.render('books/add-book', {
                 title: 'add-book',
